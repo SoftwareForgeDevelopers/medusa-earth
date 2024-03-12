9b74ba677e092c70763deb27359c49ac3983aea2
andrescaro16 <julicaro2003@gmail.com>
2024-03-12 17:55:44 -0500

    Add fonts, colors and brand logos

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "ant-regular": ["ant-regular", "sans-serif"],
+        "ant-bold": ["ant-bold", "sans-serif"],
+        "ant-light": ["ant-light", "sans-serif"],
+      },
+      colors: {
+        "var-sky-blue": "rgb(187, 255, 255)",
+        "var-light-blue": "rgb(0, 188, 255)",
+        "var-light-green": "rgb(95, 255, 255)",
+        "var-dark-green": "rgb(4, 46, 86)",
+        "var-dark-blue": "rgb(4, 20, 86)",
+        "var-green": "rgb(4, 166, 162)",
+      },
+    },
   },
   plugins: [],
 };
