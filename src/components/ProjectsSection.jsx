61117967dd2492d863afbd860fb819692b7cc488
andrescaro16 <julicaro2003@gmail.com>
2024-03-21 15:44:48 -0500

    Add base for AllProjectsSection
    Temporary layout for projectsSection component with carousel

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,46 +1,10 @@
 import React from "react";
-// import { animated, useSpring } from "react-spring";
 import ProjectCard from "./ProjectCard";
 import Label from "./Label";
 import Button from "./Button";
+import projects from "../data/projects";
 
 function ProjectsSection() {
-  // const [activeIndex, setActiveIndex] = React.useState(0);
-  const projects = [
-    {
-      totalProfit: "30%",
-      target: "$464.000.00 USD",
-      title: "Hotel Don Juan",
-      place: "Badajoz",
-      description:
-        "El proyecto solar favorito de autoconsumo en la fábrica de Alvarez Camacho, ubicado en Machena Sevilla, es una inversion viable y...",
-      image: "/assets/projects/project2.webp",
-    },
-    {
-      totalProfit: "100%",
-      target: "$354.000.00 USD",
-      title: "Alvarez Camacho",
-      place: "Marchena, Sevilla, España",
-      description:
-        "El proyecto solar favorito de autoconsumo en la fábrica de Alvarez Camacho, ubicado en Machena Sevilla, es una inversion viable y...",
-      image: "/assets/projects/project1.webp",
-    },
-    {
-      totalProfit: "77%",
-      target: "$354.000.00 USD",
-      title: "SD Huesca",
-      place: "Badajoz",
-      description:
-        "El proyecto solar favorito de autoconsumo en la fábrica de Alvarez Camacho, ubicado en Machena Sevilla, es una inversion viable y...",
-      image: "/assets/projects/project3.webp",
-    },
-  ];
-
-  // const props = useSpring({
-  //   opacity: 1,
-  //   from: { opacity: 0 },
-  //   config: { tension: 280, friction: 60 },
-  // });
 
   return (
     <section className="flex flex-col my-28 rounded-ss-70 rounded-ee-70 py-28 px-16 gap-3 bg-center bg-cover bg-[url('/assets/backgrounds/blue-for-projects.webp')] bg-blend-luminosity">
@@ -72,37 +36,12 @@ function ProjectsSection() {
       </div>
 
       <div className="flex justify-center h-full w-full gap-6 relative">
-        {/* {projects.map((project, index) => (
-          <animated.div
-            style={props}
-            key={index}
-            className={`absolute top-0 left-0 transition-transform transform ${
-              activeIndex === index ? "translate-x-0" : "translate-x-full"
-            }`}>
-            <ProjectCard project={project} />
-          </animated.div>
-        ))} */}
-        {projects.map((project, index) => (
+        {projects.slice(0, 3).map((project, index) => (
           <ProjectCard key={index} project={project} />
         ))}
       </div>
 
-      <div>
-        {/* <button
-          onClick={() =>
-            setActiveIndex(
-              (prev) => (prev - 1 + projects.length) % projects.length,
-            )
-          }>
-          Anterior
-        </button>
-        <button
-          onClick={() =>
-            setActiveIndex((prev) => (prev + 1) % projects.length)
-          }>
-          Siguiente
-        </button> */}
-      </div>
+      <div></div>
     </section>
   );
 }
