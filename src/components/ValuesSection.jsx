714a2d6b4570584d0450b5c3fee0794f8a15d1c6
andrescaro16 <julicaro2003@gmail.com>
2024-03-24 15:30:43 -0500

    Make ValueSection responsive
    Hover effects added too

--- src/components/ValuesSection.jsx
+++ src/components/ValuesSection.jsx
@@ -3,13 +3,13 @@ import Label from "./Label";
 
 function ValuesSection() {
   return (
-    <div className="flex flex-col gap-[128px] py-28 px-16">
+    <div className="flex flex-col py-28 px-16 gap-9 select-none tablet:gap-[80px]">
       <div className="flex flex-col items-center">
         <Label text="Valores" />
-        <h3 className="text-[60px] text-var-dark-blue font-w-bold">
-          Energia renovable:
+        <h3 className="text-[40px] text-var-dark-blue font-w-bold text-center tablet:text-[60px]">
+          Energía renovable:
         </h3>
-        <p className="text-[60px] text-var-dark-blue">
+        <p className="text-[40px] text-var-dark-blue text-center tablet:text-[60px]">
           Tu Puente hacia un Futuro Sostenible
         </p>
       </div>
