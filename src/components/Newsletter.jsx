889be80237c1517852eea73b7e0ee1f0d5b02ba4
andrescaro16 <julicaro2003@gmail.com>
2024-03-25 01:10:12 -0500

    Make Newsletter responsive

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -3,41 +3,43 @@ import Button from "./Button";
 
 function Newsletter() {
   return (
-    <div className="flex items-center justify-center py-16 mx-16 gap-16 bg-gray-100 p-8 rounded-[28px]">
+    <div className="flex flex-col items-center justify-center py-16 mx-7 gap-[21px] bg-gray-100 p-8 rounded-[28px] phone:mx-16 tablet:flex-row tablet:gap-0 desktop:gap-16">
       <div>
         <img
           src="/assets/logo/m-azul-overlay-sin-fondo.webp"
           alt="Logo de Medusa"
-          className="w-[338px]"
+          className="w-[325px] phone:w-[250px] max-w-[338px] laptop:w-[325px] desktop:min-w-[338px]"
         />
       </div>
-      <div className="flex flex-col gap-6">
-        <div className="flex flex-col gap-3 px-10">
-          <h2 className="text-[45px] font-w-bold text-var-dark-blue">
+      <div className="flex flex-col w-full tablet:px-7 desktop:px-10 gap-6">
+        <div className="flex flex-col w-full gap-3 text-center tablet:text-left">
+          <h2 className="text-[30px] font-w-bold text-var-dark-blue tablet:text-[45px]">
             Una dosis semanal de buenas noticias
           </h2>
-          <p className="text-[18px]">
+          <p className="text-[16px] tablet:text-[18px]">
             Si tenés más preguntas o necesitas clarificaciones adicionales,
             nuestro equipo está listo para ofrecerte la información que
             necesitas.
           </p>
         </div>
-        <form className="flex px-10 gap-3">
+        <form className="flex flex-col w-full gap-[21px] tablet:gap-3 tablet:flex-row">
           <input
             id="name"
             type="text"
             placeholder="Nombre y Apellido"
             autoComplete="name"
-            className="w-[260px] h-[50px] py-[14px] px-6 border border-var-dark-blue rounded-[47px]"
+            className="w-full h-[50px] py-[14px] px-6 border border-var-dark-blue rounded-[47px] desktop:max-w-[300px]"
           />
           <input
             id="email"
             type="email"
             placeholder="Email"
             autoComplete="email"
-            className="w-[260px] h-[50px] py-[14px] px-6 border border-var-dark-blue rounded-[47px]"
+            className="w-full h-[50px] py-[14px] px-6 border border-var-dark-blue rounded-[47px] desktop:max-w-[300px]"
           />
-          <Button type="dark-blue" styles="font-w-regular w-[260px]">
+          <Button
+            type="dark-blue"
+            styles="w-full font-w-regular desktop:max-w-[300px]">
             Suscribirse
             <span>
               <img
