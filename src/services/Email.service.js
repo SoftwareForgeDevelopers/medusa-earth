7bafefe41f6d0dbd82a8f820a15c0e77db8201e2
luarredo <luarredo@bancolombia.com>
2024-03-25 21:29:15 -0500

    Update API-KEY header and stringify emailData in SendEmail function

--- src/services/Email.service.js
+++ src/services/Email.service.js
@@ -2,9 +2,9 @@ export async function SendEmail(emailData){
     const response = await fetch("https://mailsender-latest.onrender.com/api/v2/mail", {
         method: 'POST',
         headers: {
-            'API-KEY': 'ENV'
+            'Content-Type': 'application/json',   
         },
-        body: emailData
+        body: JSON.stringify(emailData)
     })
     const data = await response.json()
     console.log(data)
